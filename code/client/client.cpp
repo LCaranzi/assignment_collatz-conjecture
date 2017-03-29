789b2736b00ac2e726ddb14d6115115d12b1be9d
Lorenzo Caranzi <lorenzocaranzi@gmail.com>
2017-03-29 17:15:35 +0200

    first exe

--- code/client/client.cpp
+++ code/client/client.cpp
@@ -1,46 +1,54 @@
 #include <cstdio>
 #include <yarp/os/all.h>
+#include <sstream>
 
 using namespace std;
 using namespace yarp::os;
 
 int main()
 {   
+    
+
     int id;
     int n = 0, N,T;
-
+    
+    std::ostringstream portName;
+    
     Network yarp;
     
-    Port p;
-    p.open("/client");
     printf("Inserire ID Cliente: ");
     scanf("%d",&id);
     printf("\nID Cliente: %d\n",id);
-
-    Bottle req,rep;
-    req.addInt(id);
-    req.addInt(n);
-
-    printf("Request: %s \n",req.toString().c_str());
+    portName<<"/client_"<<id;
     
+    printf("\n client ID: %s",portName.str().c_str());
+
+    Port p;
+    p.open(portName.str().c_str());
+
     while(true)
-    {   p.write(req,rep);
-    
-        printf("Reply: %s\n",rep.toString().c_str());
+    {   
+        Bottle req,rep;
+        req.addInt(id);
+        req.addInt(n);  
+          
+        p.write(req,rep);
+        printf("Request:  %s\n",req.toString().c_str());
+        printf("Received: %s\n\n",rep.toString().c_str());
         
         N = rep.get(1).asInt();
         T = rep.get(2).asInt();
     
         n = N;
         while(n>T)
-        {   
-            if(n%2==0)
+        {   if(n%2==0)
                 n = n/2;
             else
                 n = 3*n + 1;
         }
-        req.addInt(id);
-        req.addInt(N);
+        n = N;
+        
+       Time::delay(3);
     }
 
     p.close();
--- code/server/server.cpp
+++ code/server/server.cpp
@@ -7,30 +7,36 @@ using namespace yarp::os;
 
 int main()
 {   
-    std::deque<int> fifo;
-    std::deque<int>::iterator it;    
-
     Network yarp;
-
+    
+    
+    deque<int> fifo;
+    deque<int>::iterator it;
+    
     Bottle cmd,rep;
 
-    int n,t,cnt = 1, code;
-    
+    int n,t,cnt,code;
+    cnt = 1;
+    t = 1;
+
     Port p;
     p.open("/server");
 
     while(true)
-    {   Bottle cmd;
+    {   //printf("N: %d, T: %d \n",cnt,t);
+        Bottle cmd;
+        
         p.read(cmd,true);
-     
+
+        printf("Received: %s \n",cmd.toString().c_str());
         
         cnt++;        
         fifo.push_back(cnt);        
+        
         //printf("Request: %s \n",cmd.toString().c_str());
         
-        t = *fifo.begin()-1;
-        //printf("Begin: %d",n);
-        
+        t = *(fifo.end()-1)-1;
+                
         code = cmd.get(0).asInt();
    
         Bottle rep;     
@@ -39,18 +45,23 @@ int main()
         rep.addInt(t);
 
         p.reply(rep);
-        //printf("Reply: %s\n",rep.toString().c_str());
+        printf("Reply:    %s\n\n",rep.toString().c_str());
 
         it = fifo.begin();
-        
-        while (it != fifo.end())
+        while (it != fifo.end())                        //cancella elemento verificato
         {   if(*it == cmd.get(1).asInt())
-                fifo.erase (it);
+               fifo.erase(it);
             it++;
         }
-        
+            
+                      
+        printf("Fifo: ");                             //stampa Fifo
+        for (it = fifo.begin(); it!=fifo.end(); ++it)
+            printf("%d ",*it);
+        printf("\n\n");        
+       
     }
-
+    
     p.close();
     return 0;
 }
